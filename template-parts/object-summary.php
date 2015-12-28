0e5ae0e0ea7e5650dab4ead4ee510c6b0f50758c
Bearded Avenger <email@nickhaskins.com>
2015-12-28 17:36:36 -0500

    basic maps integration

--- template-parts/object-summary.php
+++ template-parts/object-summary.php
@@ -12,12 +12,13 @@
 	<div class="container">
 
 		<?php if ( 'hikes' == get_post_type() ) { ?>
-			<h3>Get There</h3>
+			
 			<div class="object-summary--lt">
+				<h3>Get There</h3>
 				<?php echo fo_get_hike_location_description();?>
 			</div>
 			<div class="object-summary--rt">
-				<iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=<?php echo fo_get_hike_gps_location();?>&key=AIzaSyAmZRRKW006CPNupNCPJx1jfVfdHiP3QvI" allowfullscreen></iframe>
+				<iframe width="100%" height="350" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=<?php echo fo_get_hike_gps_location();?>&key=AIzaSyAmZRRKW006CPNupNCPJx1jfVfdHiP3QvI" allowfullscreen></iframe>
 			</div>
 		<?php } else { ?>
 			<h3>TLDR;</h3>
