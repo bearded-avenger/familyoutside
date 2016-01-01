07e27a939b54469e0459f55980998455f31ab473
Bearded Avenger <email@nickhaskins.com>
2016-01-01 13:33:38 -0500

    favicons and analytics

--- footer.php
+++ footer.php
@@ -35,6 +35,15 @@
 <?php wp_footer();?>
 <script async="" src="//platform.twitter.com/widgets.js"></script>
 <script async="" src="//connect.facebook.net/en_US/all.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-38255798-17', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </body>
 </html>
