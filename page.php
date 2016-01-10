7fa173328cdf2de36869faa6d6b342cdb072c4a2
Bearded Avenger <email@nickhaskins.com>
2016-01-10 10:06:12 -0500

    page col

--- page.php
+++ page.php
@@ -14,17 +14,12 @@
 
 get_header(); ?>
 
-	<main id="primary" class="site-main" role="main">
+	<main class="site-main" role="main">
 
 		<?php
 		while ( have_posts() ) : the_post();
 
 			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
 
 		endwhile; // End of the loop.
 		?>
@@ -32,5 +27,4 @@ get_header(); ?>
 	</main>
 
 <?php
-get_sidebar();
 get_footer();
